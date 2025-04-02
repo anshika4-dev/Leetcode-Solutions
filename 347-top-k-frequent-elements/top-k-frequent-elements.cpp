3617d86429522e1a598d1c04f8cbfbf7bf2a98db
Anshika <2k23.csds2312144@gmail.com>
2025-04-02 16:31:57 +0530

    Time: 7 ms (17.64%) | Memory: 18 MB (30.54%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -4,13 +4,13 @@ public:
         unordered_map<int,int>m;
         for(int x:nums) m[x]++;
         priority_queue<pair<int,int>>maxHeap;
-        for(auto& x:m){
+        for(auto &x:m){
             maxHeap.push({x.second,x.first});
         }
         vector<int>result;
         while(!maxHeap.empty() && k--){
-result.push_back(maxHeap.top().second);
-maxHeap.pop();
+            result.push_back(maxHeap.top().second);
+            maxHeap.pop();
         }
         return result;
 

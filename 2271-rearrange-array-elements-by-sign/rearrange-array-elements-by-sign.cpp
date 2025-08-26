9005a91f7cb63d29dc09721fbbac5521ec1253d2
Anshika <2k23.csds2312144@gmail.com>
2025-08-26 14:28:11 +0530

    Time: 14 ms (16.78%) | Memory: 145.1 MB (9.47%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -3,11 +3,11 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         vector<int>r1;
         vector<int>r2;
-        vector<int>result;
         for(int x:nums){
             if(x>0) r1.push_back(x);
-            else r2.push_back(x);
+            if(x<0) r2.push_back(x);
         }
+        vector<int>result;
         for(int i=0;i<r1.size();i++){
             result.push_back(r1[i]);
             result.push_back(r2[i]);

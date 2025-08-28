5d77da240f2688693955038deca093566ee864da
Anshika <2k23.csds2312144@gmail.com>
2025-08-28 15:37:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.9 MB (5.70%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        bool colero=false,rorero=false;
-        int rows=matrix.size();
-        int columns=matrix[0].size();
-        for(int i=0;i<rows;i++){
-            if(matrix[i][0]==0)
-            colero=true;
-        }
-        for(int i=0;i<columns;i++){
-            if(matrix[0][i]==0)
-            rorero=true;
-        }
-        for(int i=1;i<rows;i++){
-            for(int j=1;j<columns;j++){
+        set<int>row;
+        set<int>cols;
+        int m=matrix.size();
+        int n=matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    matrix[i][0]=0;
-                    matrix[0][j]=0;
+                    row.insert(i);
+                    cols.insert(j);
                 }
             }
         }
-        for(int i=1;i<rows;i++){
-            for(int j=1;j<columns;j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0)
+        for(int i:row){
+            for(int j=0;j<n;j++){
                 matrix[i][j]=0;
             }
         }
-        if(colero){
-            for(int i=0;i<rows;i++){
-                matrix[i][0]=0;
-            }
-        }
-        if(rorero){
-            for(int i=0;i<columns;i++){
-            matrix[0][i]=0;
+        for(int j:cols){
+            for(int i=0;i<m;i++){
+                matrix[i][j]=0;
             }
         }
     }

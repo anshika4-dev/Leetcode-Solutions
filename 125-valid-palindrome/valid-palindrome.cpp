eab877990f0c15d6b646b834a719f9286c9bc204
Anshika <2k23.csds2312144@gmail.com>
2025-09-22 13:55:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (9.72%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string result="";
-        for(char c:s){
-            if(isalnum(c)) result += tolower(c);
-        }
-        string rev=result;
-        reverse(result.begin(),result.end());
-        if(result==rev) return true;
-        return false;
+        for(char c:s) if(isalnum(c)) result+=tolower(c);
+        string res=result;
+        reverse(res.begin(),res.end());
+        return res==result;
     }
 };

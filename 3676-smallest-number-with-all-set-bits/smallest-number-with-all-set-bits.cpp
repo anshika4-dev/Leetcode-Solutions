cb24393b99fef8f39eca82f9f6995f82383f5479
Anshika <2k23.csds2312144@gmail.com>
2025-10-29 09:33:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (30.21%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -1,9 +1,14 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        while(1){
-            if((n&(n+1))==0) return n;
-            n++;
+        int result=0;
+        if(n==1) return 1;
+        for(int i=0;i<n;i++){
+            if(pow(2,i)>n){
+                result=i;
+                break;
+            } 
         }
+        return pow(2,result)-1;
     }
 };

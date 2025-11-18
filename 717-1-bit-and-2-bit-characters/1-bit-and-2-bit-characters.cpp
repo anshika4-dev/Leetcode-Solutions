b44c8f36bf235affc88349fe3695258311ad9948
Anshika <2k23.csds2312144@gmail.com>
2025-11-18 09:59:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (8.09%) - LeetSync

--- 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool isOneBitCharacter(vector<int>& bits) {
-        int n=bits.size();
-        for(int i=0;i<n;){
-            if(i==n-1 && bits[i]==0) return true;
+        for(int i=0;i<bits.size();){
+            if(i==bits.size()-1 && bits[i]==0) return true;
             if(bits[i]==0) i++;
             else i+=2;
         }

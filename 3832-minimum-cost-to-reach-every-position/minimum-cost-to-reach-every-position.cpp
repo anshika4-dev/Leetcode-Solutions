73c5fa8f5dd3ae8bb407a7e2ed0e8cad87b46b7e
Anshika <2k23.csds2312144@gmail.com>
2025-11-19 22:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (33.01%) - LeetSync

---
+++ 3832-minimum-cost-to-reach-every-position/minimum-cost-to-reach-every-position.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> minCosts(vector<int>& cost) {
+        vector<int>result;
+        result.push_back(cost[0]);
+        for(int i=1;i<cost.size();i++){
+            if(cost[i]>result[i-1]) result.push_back(result[i-1]);
+            else result.push_back(cost[i]);
+        }
+        return result;
+    }
+};

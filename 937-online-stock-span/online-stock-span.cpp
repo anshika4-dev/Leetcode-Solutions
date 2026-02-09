6a9ec83dc1caca9ebac70fee242af9c66fef33e0
Anshika <2k23.csds2312144@gmail.com>
2026-02-09 14:28:11 +0530

    Time: 32 ms (54.67%) | Memory: 92 MB (89.19%) - LeetSync

--- 937-online-stock-span/online-stock-span.cpp
+++ 937-online-stock-span/online-stock-span.cpp
@@ -7,7 +7,7 @@ public:
     
     int next(int price) {
         int day=1;
-        while(!st.empty() && st.top().first <= price){
+        while(!st.empty() && st.top().first <=price){
             day+=st.top().second;
             st.pop();
         }

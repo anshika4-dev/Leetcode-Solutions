962a671a9923946db56cc942f88947685fb9c201
Anshika <2k23.csds2312144@gmail.com>
2026-02-13 14:48:20 +0530

    Time: 12 ms (36.37%) | Memory: 21.8 MB (11.59%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m=matrix.size(), n=matrix[0].size();
+        int max_count=0;
+        vector<vector<int>> v(m,vector<int>(n,0));
+        for(int i=0;i<n;i++) v[0][i]=(matrix[0][i]-'0');
+        for(int i=1;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1') v[i][j]=v[i-1][j]+1;
+            }
+        }
+        for(int i=0;i<m;i++){
+            vector<int>left(n,-1), right(n,n);
+            stack<int>st;
+            for(int j=0;j<n;j++){
+                while(!st.empty() && v[i][st.top()]>=v[i][j]) st.pop();
+                left[j]=!st.empty()?st.top():-1;
+                st.push(j);
+            }
+            while(!st.empty()) st.pop();
+            for(int j=n-1;j>=0;j--){
+                while(!st.empty() && v[i][st.top()]>=v[i][j]) st.pop();
+                right[j]=!st.empty()?st.top():n;
+                st.push(j);
+            }
+            for(int j=0;j<n;j++) max_count=max(max_count,(right[j]-left[j]-1)*v[i][j]);
+        }
+        return max_count;
+
+    }
+};

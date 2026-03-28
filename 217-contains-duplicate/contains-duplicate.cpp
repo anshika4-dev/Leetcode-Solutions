d4328a4223665d1582427d97ceb5d486813cc5cb
Anshika <2k23.csds2312144@gmail.com>
2026-03-29 00:22:57 +0530

    Time: 47 ms (65.17%) | Memory: 90.7 MB (69.78%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>m;
+        for(int x:nums){
+            if(m[x]>0) return true;
+            m[x]++;
+        }
+        return false;
+    }
+};

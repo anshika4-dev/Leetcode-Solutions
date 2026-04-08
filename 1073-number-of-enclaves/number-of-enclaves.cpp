273b920163cfaf6b5f9fc4d4c46fdd6d2ee6f3df
Anshika <2k23.csds2312144@gmail.com>
2026-04-08 15:59:54 +0530

    Time: 88 ms (7.42%) | Memory: 51 MB (7.16%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void bfs(vector<vector<int>> &grid, int i,int j){
+        queue<pair<int,int>>q;
+        vector<vector<int>> dir={{0,1},{0,-1},{1,0},{-1,0}};
+        q.push({i,j});
+        while(!q.empty()){
+            int x=q.front().first,y=q.front().second;
+            q.pop();
+            if(x<0 || x>=grid.size() || y<0 || y>=grid[0].size() || grid[x][y]==0) continue;
+            grid[x][y]=0;
+            for(auto a:dir){
+                q.push({x+a[0],y+a[1]});
+            }
+        }
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m=grid.size(),n=grid[0].size(),count=0;
+        for(int i=0;i<m;i++) if(grid[i][0]==1) bfs(grid,i,0);
+        for(int i=0;i<m;i++) if(grid[i][n-1]==1) bfs(grid,i,n-1);
+        for(int i=0;i<n;i++) if(grid[0][i]==1) bfs(grid,0,i);
+        for(int i=0;i<n;i++) if(grid[m-1][i]==1) bfs(grid,m-1,i);
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++) if(grid[i][j]==1) count++;
+        return count;
+    }
+};

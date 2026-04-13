861cbdb20356c842e5f945d0f49d0bce28982d14
Anshika <2k23.csds2312144@gmail.com>
2026-04-13 12:33:08 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (78.66%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&grid, int count,int i,int j){
+    int dfs(vector<vector<int>> &grid, int i, int j, int &count){
         if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]==0) return 0;
+        count++;
         grid[i][j]=0;
-        return 1+dfs(grid,count,i-1,j)+dfs(grid,count,i+1,j)+dfs(grid,count,i,j+1)+dfs(grid,count,i,j-1);
+        dfs(grid,i+1,j,count);
+        dfs(grid,i-1,j,count);
+        dfs(grid,i,j+1,count);
+        dfs(grid,i,j-1,count);
+        return count;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxCount=0, m=grid.size(),n=grid[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) maxCount=max(maxCount,dfs(grid,0,i,j));
-            }
+        int count=0,area=0,m=grid.size(),n=grid[0].size();
+        for(int i=0;i<m;i++) for(int j=0;j<n;j++) if(grid[i][j]==1){ 
+            area=max(area,dfs(grid,i,j,count));
+            count=0;
         }
-        return maxCount;
+        return area;
     }
 };

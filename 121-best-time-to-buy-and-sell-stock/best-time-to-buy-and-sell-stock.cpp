83d968278ce116e78c4aa93b95540e0f85df6d47
Anshika <2k23.csds2312144@gmail.com>
2026-06-21 01:20:35 +0530

    Time: 3 ms (28.30%) | Memory: 97.4 MB (24.52%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int mini=INT_MAX,maxi=INT_MIN;
         for(int i=0;i<prices.size();i++){
             mini=min(mini,prices[i]);
-            maxi=max(prices[i]-mini,maxi);
+            maxi=max(maxi,prices[i]-mini);
         }
         return maxi;
     }

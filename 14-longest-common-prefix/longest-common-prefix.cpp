dc85264a1761251de6cc8f2938e36b39089535f1
Anshika <2k23.csds2312144@gmail.com>
2026-06-21 00:39:21 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (26.96%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,11 +3,11 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string result=strs[0];
         for(int i=1;i<strs.size();i++){
-            string word=strs[i];
             int j=0;
-            for(;j<min(result.length(),word.length());j++) if(word[j]!=result[j]) break;
-            result = word.substr(0,j);
-            if(result.length()==0) break;
+            string words=strs[i];
+            for(;j<min(result.size(),words.size());j++) if(result[j]!=words[j]) break;
+            result=words.substr(0,j);
+            if(result.size()==0) break;
         }
         return result;
     }

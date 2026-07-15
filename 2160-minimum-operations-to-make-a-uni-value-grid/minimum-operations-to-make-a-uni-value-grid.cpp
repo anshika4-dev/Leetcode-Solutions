0a78871a87ef7400541cc222e3124992d922bcdd
Anshika <2k23.csds2312144@gmail.com>
2026-07-15 11:03:32 +0530

    Time: 44 ms (37.14%) | Memory: 87.2 MB (50.38%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
-    int minOperations(vector<vector<int>>& grid, int x) {
-        int n = grid.size(), m = grid[0].size();
-        int N = n * m;
-        int freq[10001] = {0};
-        int mn = grid[0][0], mx = mn;
-
-        for (const auto& row : grid) {
-            for (int val : row) {
-                if ((val - grid[0][0]) % x != 0) return -1;
-                freq[val]++;
-                mn = min(mn, val);
-                mx = max(mx, val);
-            }
+    int minOperations(vector<vector<int>>& grid, int k) {
+        vector<int>temp;
+        for(auto x:grid) for(int a:x) temp.push_back(a);
+        sort(temp.begin(),temp.end());
+        int median=temp[temp.size()/2],count=0;
+        for(int i=0;i<temp.size();i++){
+            if(abs(temp[i]-median)%k!=0) return -1;
+            count+=(abs(temp[i]-median)/k);
         }
-
-        int target = (N + 1) / 2;
-        int acc = 0, median = mn;
-
-        for (int i = mn; i <= mx; i += x) {
-            acc += freq[i];
-            if (acc >= target) {
-                median = i;
-                break;
-            }
-        }
-
-        int ops = 0;
-        for (int i = mn; i <= mx; i += x)
-            ops += abs(i - median) / x * freq[i];
-
-        return ops;
+        return count;
     }
 };

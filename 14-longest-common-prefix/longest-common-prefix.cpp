b0c8c7512b40184014e4752dc3af3da52811779a
Anshika <2k23.csds2312144@gmail.com>
2026-07-18 20:36:53 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (39.71%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -4,10 +4,10 @@ public:
         string result=strs[0];
         for(int i=1;i<strs.size();i++){
             int j=0;
-            string words=strs[i];
-            for(;j<min(result.size(),words.size());j++) if(result[j]!=words[j]) break;
-            result=words.substr(0,j);
-            if(result.size()==0) break;
+            string word=strs[i];
+            for(;j<min(result.size(),word.size());j++) if(result[j]!=word[j]) break;
+            result=word.substr(0,j);
+            if(result.length()==0) return "";
         }
         return result;
     }

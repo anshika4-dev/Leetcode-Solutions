27e4696bedfa7cef74d575fddab31b75c019afdb
Anshika <2k23.csds2312144@gmail.com>
2026-07-30 10:14:26 +0530

    Time: 514 ms (52.37%) | Memory: 0B (100.00%) - LeetSync

--- 1179-game-play-analysis-i/game-play-analysis-i.sql
+++ 1179-game-play-analysis-i/game-play-analysis-i.sql
@@ -1,3 +1,4 @@
-select player_id,min(event_date) as first_login
-from Activity
-group by player_id
+# Write your MySQL query statement below
+SELECT player_id,MIN(event_date) as first_login
+FROM Activity
+GROUP BY player_id;

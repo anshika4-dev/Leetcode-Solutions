4944e61c658519d8c74b83b3557f4c52133bfdaa
Anshika <2k23.csds2312144@gmail.com>
2026-08-03 15:09:39 +0530

    Time: 3 ms (54.82%) | Memory: 32.5 MB (98.69%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int helper(vector<int>& nums,int goal){
-        long long left=0,right=0,sum=0,res=0;
-        while(right<nums.size()){
-            sum+=nums[right];
-            while(left<=right && sum>goal){
-                sum-=nums[left];
+    int helper(vector<int>& nums, int goal){
+        int curr=0,left=0,res=0;
+        for(int right=0;right<nums.size();right++){
+            curr+=nums[right];
+            while(left<=right && curr>goal){
+                curr-=nums[left];
                 left++;
             }
-            res+=right-left+1;
-            right++;
+            res+=(right-left+1);
         }
         return res;
     }

b25a48cb70b7161809e0eee86889788b5ca1fbf8
Anshika <2k23.csds2312144@gmail.com>
2026-08-03 14:23:28 +0530

    Time: 98 ms (73.10%) | Memory: 98.4 MB (30.24%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        long long res=0,curr=0,left=0;
+        long long left=0,curr=0,res=0;
         unordered_map<int,int>m;
         for(int right=0;right<nums.size();right++){
-            curr+=nums[right];
             m[nums[right]]++;
+            curr+=nums[right];
             while(m[nums[right]]>1){
                 m[nums[left]]--;
                 curr-=nums[left];
                 left++;
             }
             if(right-left+1==k){
-                res=max(curr,res);
-                curr-=nums[left];
+                res=max(res,curr);
                 m[nums[left]]--;
+                curr-=nums[left];
                 left++;
             }
+
         }
         return res;
     }

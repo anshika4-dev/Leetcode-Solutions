e31fd045022de055c85bfdeab9d288ba4b634807
Anshika <2k23.csds2312144@gmail.com>
2026-08-13 10:33:20 +0530

    Time: 47 ms (7.44%) | Memory: 18.4 MB (25.97%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>&dp, int i,int j, string &s1, string &s2){
+        if(i==s1.size() && j==s2.size()) return 0;
+        if(i==s1.size()) return int(s2[j])+solve(dp,i,j+1,s1,s2);
+        if(j==s2.size()) return int(s1[i])+solve(dp,i+1,j,s1,s2);
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1[i]==s2[j]) return dp[i][j]=solve(dp,i+1,j+1,s1,s2);
+        return dp[i][j]=min(int (s1[i])+solve(dp,i+1,j,s1,s2),int (s2[j])+solve(dp,i,j+1,s1,s2));
+
+    }
     int minimumDeleteSum(string s1, string s2) {
-       vector<vector<int>>dp(s1.length()+1,vector<int>(s2.length()+1,0));
-       for(int i=1;i<=s1.length();i++) dp[i][0]=int(s1[i-1])+dp[i-1][0];
-       for(int i=1;i<=s2.length();i++) dp[0][i]=int(s2[i-1])+dp[0][i-1];
-       dp[0][0]=0;
-       for(int i=1;i<=s1.length();i++){
-        for(int j=1;j<=s2.length();j++){
-            if(s1[i-1]==s2[j-1]) dp[i][j]=dp[i-1][j-1];
-            else dp[i][j]+=int(min(dp[i-1][j]+int(s1[i-1]),dp[i][j-1]+int(s2[j-1])));
-        }
-       }
-       return dp[s1.length()][s2.length()];
+        vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
+        return solve(dp,0,0,s1,s2);
     }
 };

b7495029119b77b050e8e6ec22a90aa6ffce868e
Anshika <2k23.csds2312144@gmail.com>
2026-08-20 11:52:15 +0530

    Time: 51 ms (12.26%) | Memory: 78.7 MB (39.90%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int>temp;
-        for(int i=0;i<grid.size();i++) for(int j=0;j<grid[0].size();j++) temp.push_back(grid[i][j]);
-        sort(temp.begin(),temp.end());
-        int median=temp[temp.size()/2];
-        int count=0;
-        for(int i=0;i<temp.size();i++){
-            int diff=abs(temp[i]-median);
+        vector<int>res;
+        for(int i=0;i<grid.size();i++) for(int j=0;j<grid[0].size();j++) res.push_back(grid[i][j]);
+        sort(res.begin(),res.end());
+        int count=0,median=res[res.size()/2];
+        for(int i=0;i<res.size();i++){
+            int diff=abs(res[i]-median);
             if(diff%x!=0) return -1;
             count+=(diff/x);
         }

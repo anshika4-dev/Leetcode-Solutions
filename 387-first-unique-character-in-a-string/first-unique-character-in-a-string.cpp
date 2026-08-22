0b24cbf54bedb32d99a505231faf2b63c88f805d
Anshika <2k23.csds2312144@gmail.com>
2026-08-22 12:25:27 +0530

    Time: 19 ms (22.77%) | Memory: 14.9 MB (38.72%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -3,11 +3,7 @@ public:
     int firstUniqChar(string s) {
         unordered_map<char,int>m;
         for(char x:s) m[x]++;
-        int i=0;
-        for(char x:s){
-            if(m[x]==1) return i;
-            i++;
-        }
+        for(int i=0;i<s.size();i++) if(m[s[i]]==1) return i;
         return -1;
     }
 };

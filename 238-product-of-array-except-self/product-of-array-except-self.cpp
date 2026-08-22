3796938b989cea5445e9de60161e995a4fd0bf84
Anshika <2k23.csds2312144@gmail.com>
2026-08-22 12:04:27 +0530

    Time: 3 ms (37.23%) | Memory: 40.1 MB (81.35%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int count=0;
-        for(int x:nums) if(x==0) count++;
-        if(count>=2){
-            for(int i=0;i<nums.size();i++) nums[i]=0;
-            return nums;
-        }
-        int prod=1;
+        int count=0,mul=1;
+        vector<int>res(nums.size(),0);
         for(int i=0;i<nums.size();i++){
-                if(nums[i]==0) continue;
-                prod*=nums[i];
+            if(nums[i]==0){
+                count++;
+                continue;
             }
-        if(count==1){
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]==0) nums[i]=prod;
-                else nums[i]=0;
-            }
-                            return nums;
+            mul*=nums[i];
         }
-        for(int i=0;i<nums.size();i++){
-            nums[i]=prod/nums[i];
+        if(count>1) return res;
+        if(count==1) for(int i=0;i<nums.size();i++) if(nums[i]==0){
+            res[i]=mul;
+            return res;
         }
-        return nums;
+        for(int i=0;i<nums.size();i++) res[i]=mul/nums[i];
+        return res;
     }
 };

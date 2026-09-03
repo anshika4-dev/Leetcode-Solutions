e7821a36b199d76cda264b13b92934971aa699d4
Anshika <2k23.csds2312144@gmail.com>
2026-09-03 15:08:00 +0530

    Time: 1 ms (29.63%) | Memory: 19.4 MB (7.04%) - LeetSync

--- 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
+++ 2284-smallest-value-of-the-rearranged-number/smallest-value-of-the-rearranged-number.py
@@ -6,9 +6,6 @@ class Solution:
             return -int(s)
         s=sorted(list(str(num)))
         i=0
-        while s[i]=='0':
-            i+=1
+        while s[i]=='0': i+=1
         s[0],s[i]=s[i],s[0]
         return int("".join(s))
-
-        

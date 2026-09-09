d2e0609f0ce0829c4c597316d71da772b123f8dc
Anshika <2k23.csds2312144@gmail.com>
2026-09-09 16:08:33 +0530

    Time: 3 ms (88.14%) | Memory: 13.2 MB (27.40%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        int left=0,res=0,curr=0;
-        for(int right=0;right<s.size();right++){
-            if(s[right]=='a' || s[right]=='e' || s[right]=='i' || s[right]=='o' || s[right]=='u') curr++;
+        int left=0,count=0,result=0;
+        string vowel="aeiou";
+        for(int right=0;right<ssize(s);right++){
+            if(vowel.find(s[right])!=string::npos) count++;
             if(right-left+1==k){
-                res=max(res,curr);
-                if(s[left]=='a' || s[left]=='e' || s[left]=='i' || s[left]=='o' || s[left]=='u') curr--;
+                result=max(result,count);
+                if(vowel.find(s[left])!=string::npos) count--;
                 left++;
             }
         }
-        return res;
+        return result;
     }
 };

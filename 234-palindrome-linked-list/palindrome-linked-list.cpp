6abbc62169e526924930f3ed9a2107e2f80d0b7c
Anshika <2k23.csds2312144@gmail.com>
2026-09-14 15:08:55 +0530

    Time: 3 ms (58.50%) | Memory: 117.9 MB (81.63%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,42 +1,49 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-    ListNode* reverse(ListNode* head){
-        ListNode* prev=nullptr, *curr=head;
-        while(curr){
-            ListNode* next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
+
         return prev;
     }
+
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next) return head;
-        ListNode* slow=head,*fast=head,*prev=head;
-        while(fast && fast->next){
-            prev=slow;
-            slow=slow->next;
-            fast=fast->next->next;
+        if (!head || !head->next)
+            return true;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        if(fast) slow=slow->next;
-        prev->next=reverse(slow);
-        ListNode* temp=head;
-        slow=prev->next;
-        while(slow){
-            if(slow->val!=temp->val) return false;
-            slow=slow->next;
-            temp=temp->next;
+
+        // Odd length → skip middle node
+        if (fast)
+            slow = slow->next;
+
+        // Reverse second half
+        slow = reverse(slow);
+
+        // Compare
+        ListNode* temp = head;
+
+        while (slow) {
+            if (temp->val != slow->val)
+                return false;
+
+            temp = temp->next;
+            slow = slow->next;
         }
+
         return true;
     }
 };
